dd6ae18ab8c80828dfbcc37047f225b178bdee45
Subroto Kumar <152253595+SubrotoKumar7@users.noreply.github.com>
2025-09-22 16:02:24 +0600

    bug fixed

--- src/components/selected/Selected.jsx
+++ src/components/selected/Selected.jsx
@@ -1,7 +1,7 @@
 const Selected = () => {
     return (
         <div className="w-11/12 mx-auto">
-            <h1>Selected Players</h1>
+            <h1>I am from Selected Components</h1>
         </div>
     );
 };
--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -1,7 +1,7 @@
 const Title = ({toggleBtn, toggle}) => {
     return (
         <div className="w-11/12 mx-auto flex justify-between items-center">
-            <h1 className="font-bold">Available Players</h1>
+            <h1 className="font-bold">{toggle ? "Available Players" : "Selected Players"}</h1>
             <div className="flex">
                 <button className={` ${toggle ? "bg-[#E7FE29]" : "" } cursor-pointer font-medium py-2 px-3 border border-gray-400 border-r-0 rounded-l-2xl`} onClick={()=> toggleBtn(true)}>Available</button>
                 <button className={` ${toggle ? "" : "bg-[#E7FE29]"} cursor-pointer py-2 px-3 border border-gray-400 border-l-0 rounded-r-2xl`} onClick={()=> toggleBtn(false)}>Selected (0)</button>
